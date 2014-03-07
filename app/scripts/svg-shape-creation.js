0a866d7afa4ed89d3bcb8691120b476fb910dfac
Daniela Baron <baron.daniela@gmail.com>
2014-03-07 12:48:39 -0500

    fix range array error

--- app/scripts/svg-shape-creation.js
+++ app/scripts/svg-shape-creation.js
@@ -20,11 +20,11 @@ d3.csv('/data/tuts.csv', function(d) {d.year = parse(d.year); return d;}, functi
   // going to create dots connected by lines
   var yearScale = d3.time.scale()
     .domain(d3.extent(data, function(d) {return d.year;} )) // extent returns array with min and max from input
-    .range(50, window.innerWidth - 50);
+    .range([50, window.innerWidth - 50]);
 
   var numberScale = d3.scale.linear()
     .domain([0, d3.max(data, function(d) { return d.number; })])
-    .range(50, window.innerHeight - 50);
+    .range([50, window.innerHeight - 50]);
 
   // not working for me, getting NaN from yearScale and numberScale
   svg.selectAll('circle')
